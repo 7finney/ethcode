72b366768ecf32d79878f1769ddf949aa76ff113
Ayan Banerjee <ayanb1999@gmail.com>
2019-05-07 17:04:17 +0530

    Fixed github import bug

--- test/res/greeter.sol
+++ test/res/greeter.sol
@@ -1,5 +1,7 @@
 pragma solidity >=0.5.0 <0.6.0;
-import "./mortal.sol";
+ // import "./mortal.sol";
+ import "github.com/mathcody/solidity-examples/mortal/mortal.sol";
+
 
 contract Greeter is Mortal {
     /* Define variable greeting of the type string */
