cf95d547f0d09f0d19ff48b236d64aa6c04dcfd3
blue <soumyaniljana@localhost.localdomain>
2019-11-08 19:22:38 +0530

    adding modification to vyper compiler

--- ext-src/vyp-worker.ts
+++ ext-src/vyp-worker.ts
@@ -1,20 +1,36 @@
 // @ts-ignore
 import * as shell from "shelljs";
+import * as fs from "fs";
+import { ISources } from "./types";
 
-function compileVyper(contractFile: any) {
-    var args: string = "vyper " + contractFile;
+function compileVyper(source: ISources) {
+    var input_json = {
+        "language": "Vyper",
+        "sources": source,
+        "settings": {
+            "evmVersion": "byzantium"
+        },
+        "outputSelection": {
+            "*": ["evm.bytecode", "abi"],
+            // @ts-ignore
+            [source.fn]: ["ast"]
+        }
+    };
+    fs.writeFileSync(__dirname + "/" + "temp-vy.json", JSON.stringify(input_json, null, 4), 'UTF-8');
+    var args: string = "vyper-json " + __dirname + "/" + "temp-vy.json";
     var escaped = shell.exec(args);
     var m: object = {
         "message": "compilation success",
         "compiled": escaped
     }
+    // fs.unlink("temp-vy.json", ()=>{});
     // @ts-ignore
     process.send(m);
 }
 
 // @ts-ignore
 process.on('message', (m) => {
-    if(m.command == "compile") {
-        compileVyper(m.File);
+    if (m.command == "compile") {
+        compileVyper(m.src);
     }
 })
