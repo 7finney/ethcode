c1be7198323609d817d9cdc482760d166c254503
blue <soumyaniljana@localhost.localdomain>
2019-11-25 19:37:50 +0530

    adding basic constructor ui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,6 +8,7 @@ import {
 } from "../actions";
 import "./App.css";
 import ContractCompiled from "./ContractCompiled";
+import ContractDeploy from "./ContractDeploy";
 import Dropdown from "./Dropdown";
 import CompilerVersionSelector from "./CompilerVersionSelector";
 import TestDisplay from "./TestDisplay";
@@ -200,6 +201,38 @@ class App extends Component<IProps, IState> {
                   );
                 }
               )}
+            </div>
+          )}
+          {compiled && fileName && (
+            <div className="compiledOutput">
+              {
+                Object.keys(compiled.contracts[fileName]).map((contractName: string, i: number) => {
+                  const bytecode =
+                      compiled.contracts[fileName][contractName].evm.bytecode
+                        .object;
+                    const ContractABI =
+                      compiled.contracts[fileName][contractName].abi;
+                  return(
+                    <div
+                      id={contractName}
+                      className="contract-container"
+                      key={i}
+                    >
+                      {
+                        <ContractDeploy
+                          contractName={contractName}
+                          bytecode={bytecode}
+                          abi={ContractABI}
+                          vscode={vscode}
+                          compiled={compiled}
+                          errors={error}
+                        />
+                      }
+                    </div>
+                  );
+
+                })
+              }
             </div>
           )}
         </p>
--- src/components/ContractCompiled/ContractCompiled.tsx
+++ src/components/ContractCompiled/ContractCompiled.tsx
@@ -9,74 +9,26 @@ interface IProps {
   compiled: any;
   errors: any;
 }
-interface IState {
-  gasSupply: number;
-  errors: any
-}
-class ContractCompiled extends Component<IProps, IState> {
-  public state: IState = {
-    gasSupply: 1500000,
-    errors: undefined
-  };
-  constructor(props: IProps, state: IState) {
+class ContractCompiled extends Component<IProps> {
+  constructor(props: IProps) {
     super(props);
-    this.handleDeploy = this.handleDeploy.bind(this);
-  }
-  
-  public handleDeploy() {
-    const { vscode, bytecode, abi } = this.props;
-    const { gasSupply } = this.state;
-    vscode.postMessage({
-      command: "run-deploy",
-      payload: {
-        abi,
-        bytecode,
-        gasSupply
-      }
-    });
-  }
-  componentDidUpdate(prevProps: any) {
-    if(this.props.errors !== prevProps.errors) {
-      const { errors } = this.props;
-      if(this.props.errors) this.setState({ errors: errors });
-    }
-  }
-
-  handleChange(event: any) {
-    this.setState({ gasSupply: event.target.value });
   }
   
   public render() {
     const { contractName, bytecode, abi } = this.props;
-    const { gasSupply, errors } = this.state;
+    console.log("after compilation")
+    console.dir(bytecode)
+    console.log(typeof abi);
     return (
       <div>
         <span className="contract-name inline-block highlight-success">
           Contract Name: {contractName}
         </span>
-
         <div className="byte-code">
           <pre className="large-code">{JSON.stringify(bytecode)}</pre>
         </div>
         <div className="abi-definition">
           <pre className="large-code">{JSON.stringify(abi)}</pre>
-        </div>
-        <div>
-          <form onSubmit={this.handleDeploy}>
-            <label>
-              Gas Supply:
-              <input type="number" value={gasSupply} id="deployGas" onChange={(e) => this.handleChange(e)}/>
-            </label>
-            <input type="submit" value="Deploy" />
-          </form>
-        </div>
-        <div>
-          {
-            errors &&
-            <div>
-              <pre>{JSON.stringify(errors)}</pre>
-            </div>
-          }
         </div>
       </div>
     );
