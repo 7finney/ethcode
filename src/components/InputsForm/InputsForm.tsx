94eed16e5b7f3100eb8cb714a1d6d2f16e4bea72
Ayan Banerjee <ayanb1999@gmail.com>
2020-09-29 21:54:50 +0530

    inputform functional transformed

--- src/components/InputsForm/InputsForm.tsx
+++ src/components/InputsForm/InputsForm.tsx
@@ -1,45 +1,40 @@
 // @ts-ignore
-import React, { Component } from "react";
+import React from "react";
 
 interface IProps {
   contractName: string;
   abi: any;
   onSubmit: () => void;
 }
-class InputsForm extends Component<IProps> {
-  constructor(props: IProps) {
-    super(props);
-    this._handleChange = this._handleChange.bind(this);
-  }
+const InputsForm = (props: IProps) => {
+  const { contractName, abi } = props;
 
-  public _handleChange(input: any, event: any) {
+  const handleChange = (input: any, event: any) => {
+    // eslint-disable-next-line no-param-reassign
     input.value = event.target.value;
-  }
+  };
 
-  public render() {
-    const { contractName, abi } = this.props;
-
-    return (
-      <div id={`${contractName}_inputs`}>
-        {abi.type === "constructor" &&
-          abi.inputs.map((input: any, i: string) => {
-            return (
-              <form key={i} onSubmit={this.props.onSubmit}>
-                <button className="input text-subtle">{input.name}</button>
-                <input
-                  id={i}
-                  type="text"
-                  className="inputs"
-                  placeholder={input.type}
-                  value={input.value}
-                  onChange={(e) => this._handleChange(input, e)}
-                />
-              </form>
-            );
-          })}
-      </div>
-    );
-  }
-}
+  return (
+    <div id={`${contractName}_inputs`}>
+      {abi.type === "constructor" &&
+        abi.inputs.map((input: any, i: string) => {
+          return (
+            // eslint-disable-next-line react/no-array-index-key
+            <form key={i} onSubmit={props.onSubmit}>
+              <button className="input text-subtle">{input.name}</button>
+              <input
+                id={i}
+                type="text"
+                className="inputs"
+                placeholder={input.type}
+                value={input.value}
+                onChange={(e) => handleChange(input, e)}
+              />
+            </form>
+          );
+        })}
+    </div>
+  );
+};
 
 export default InputsForm;
