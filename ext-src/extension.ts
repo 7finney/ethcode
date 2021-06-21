b7c09679fcb92181280a03e092ed81fb9a108857
0mkar <0mkar@protonmail.com>
2021-06-21 21:53:20 +0530

    create account in native vscode

--- ext-src/extension.ts
+++ ext-src/extension.ts
@@ -1,5 +1,8 @@
 // @ts-ignore
 import * as vscode from 'vscode';
+import * as path from 'path';
+import { InputBoxOptions } from 'vscode';
+import { fork, ChildProcess } from 'child_process';
 import API from './api';
 import { ReactPanel } from './reactPanel';
 
@@ -7,7 +10,18 @@ import Logger from './utils/logger';
 
 // Create logger
 const logger = new Logger();
+const pwdInpOpt: InputBoxOptions = {
+  ignoreFocusOut: true,
+  password: true,
+  placeHolder: 'Password',
+};
 
+const createAccWorker = (): ChildProcess => {
+  // return fork(path.join(__dirname, 'accWorker.js'), [], {
+  //   execArgv: [`--inspect=${process.debugPort + 1}`],
+  // });
+  return fork(path.join(__dirname, 'accWorker.js'));
+};
 // eslint-disable-next-line import/prefer-default-export
 export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
@@ -25,6 +39,24 @@ export async function activate(context: vscode.ExtensionContext) {
         ? vscode.window.activeTextEditor.document.getText()
         : undefined;
       ReactPanel.currentPanel.sendTestContract(editorContent, fileName);
+    }),
+    vscode.commands.registerCommand('ethcode.create.account', async () => {
+      logger.log('Creating ethereum keypair...');
+      try {
+        const password = await vscode.window.showInputBox(pwdInpOpt);
+        const accWorker = createAccWorker();
+        accWorker.on('message', (m: any) => {
+          if (m.account) {
+            logger.success('Account created!');
+            logger.success(JSON.stringify(m.account));
+          } else if (m.error) {
+            logger.error(m.error);
+          }
+        });
+        accWorker.send({ command: 'create-account', pswd: password, ksPath: context.extensionPath });
+      } catch (error) {
+        logger.error(error);
+      }
     })
   );
   await ReactPanel.createOrShow(context.extensionPath);
